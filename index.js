56f5a12c69c6db8812a2ddac36222c45a776a03a
Justin Price <mr.price@Justins-iMac.local>
2020-02-11 12:19:46 -0500

    post should work, get works

--- index.js
+++ index.js
@@ -22,27 +22,27 @@ server.get('/api/users', (req, res) => {
     });
 })
 
-server.get(`/api/users/:${id}`, (req, res) => {
-    const { id } = req.params;
+// server.get(`/api/users/:${id}`, (req, res) => {
+//     const { id } = req.params;
    
-        if (!obj) {
-            res.status(404).json({ message: "The user with the specified ID does not exist." });
-        } 
-       else  Database.findById(id).then(obj => {res.status(200).json(obj); 
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: "The user information could not be retrieved." });
-    })
-})
+//         if (!obj) {
+//             res.status(404).json({ message: "The user with the specified ID does not exist." });
+//         } 
+//        else  Database.findById(id).then(obj => {res.status(200).json(obj); 
+//     }).catch(err => {
+//         console.log(err);
+//         res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+//     })
+// })
 
 server.post('/api/users', (req, res) => {
-  
-    const newHobbit = req.body;
-    if (!newHobbit.name && !newHobbit.bio){
+  console.log(req.body)
+    // const newHobbit = req.body;
+    if (!req.body.name || !req.body.bio){
             res.status(400).json({errorMessage: 'Please provide name and bio for the user.'});
         }    
-     else Database.insert(newHobbit).then(db => {
-        res.status(201).json(newHobbit)
+     else Database.insert(req.body).then(db => {
+        res.status(201).json(db)
     }).catch(err => {
         console.log(err);
         res.status(500).json({errorMessage: 'There was an error while saving the user to the database'});
@@ -50,52 +50,64 @@ server.post('/api/users', (req, res) => {
     
 })
 
-server.put('/api/users/:id', (req, res) => {
+// server.put('/api/users/:id', (req, res) => {
   
-    const {id}= req.params;
-    const {dataChanges} = res.id;
+//     const {id}= req.params;
+//     const {dataChanges} = res.id;
 
-    if (!dataChanges.id){
-        res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
-    } 
+//     if (!dataChanges.id){
+//         res
+//         .status(404)
+//         .json({ message: "The user with the specified ID does not exist." });
+//     } 
 
-    if (!dataChanges.name && !dataChanges.bio){
-        res
-        .status(400)
-        .json({ errorMessage: "Please provide name and bio for the user." });
-    }
-    else Database.update(id, dataChanges)
-    .then(db => {
-        res.status(200).json(db);
-    })
-    .catch(err => {
-        console.log(err);
-        res
-          .status(500)
-          .json({ errorMessage: "The user information could not be modified." });
+//     if (!dataChanges.name && !dataChanges.bio){
+//         res
+//         .status(400)
+//         .json({ errorMessage: "Please provide name and bio for the user." });
+//     }
+//     else Database.update(id, dataChanges)
+//     .then(db => {
+//         res.status(200).json(db);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res
+//           .status(500)
+//           .json({ errorMessage: "The user information could not be modified." });
         
-      });
-  })
+//       });
+//   })
 
 
-  server.delete('/api/users/:id', (req, res) => {
-    const deleteHobbit = req.body; 
+// server.put('./api/users/:id', (req, res)=> {
+//     const updateObject = req.body;
 
-    if (!deleteHobbit.name && !deleteHobbit.bio){
-        res
-        .status(404)
-        .json({ errorMessage: "The user with the specified ID does not exist." });
-    }
-    Database.delete(id, deleteHobbit)
-    .then(db => {
-        res.status(200).json(db);
-    })
-    .catch(err => {
-        console.log(err);
-        res
-          .status(500)
-          .json({ errorMessage: "The user could not be removed." });
-    });
-  });
+//     Database.update(req.params.id, updateObject).then(obj => {
+//         if (obj){
+//             res.status(200).json(obj);
+//         } else {
+//             res.status(400).json({errorMessage: })
+//         }
+//     })
+// })
+
+//   server.delete('/api/users/:id', (req, res) => {
+//     const deleteHobbit = req.body; 
+
+//     if (!deleteHobbit.name && !deleteHobbit.bio){
+//         res
+//         .status(404)
+//         .json({ errorMessage: "The user with the specified ID does not exist." });
+//     }
+//     Database.delete(id, deleteHobbit)
+//     .then(db => {
+//         res.status(200).json(db);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res
+//           .status(500)
+//           .json({ errorMessage: "The user could not be removed." });
+//     });
+//   });
